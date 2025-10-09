73db431b68234d7905fed0e8a289a0c051c6dd55
Abhishek Goswami <abhishekgoswami8791@gmail.com>
2025-10-09 17:43:51 +0530

    02

---
+++ 2-bharat-clock/src/components/CurrentTime.jsx
@@ -0,0 +1,12 @@
+let CurrentTime = () => {
+  let time = new Date();
+
+
+   return (
+     <p className="lead">This is the currentTime: {time.toLocaleDateString()} -{" "} 
+   {time.toLocaleTimeString()}
+   </p>
+   );
+};
+
+export default CurrentTime;
