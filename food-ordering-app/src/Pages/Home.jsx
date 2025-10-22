bae2cff90890c7144670822d7721bbd33f098349
Abhishek Goswami <abhishekgoswami8791@gmail.com>
2025-10-22 16:03:39 +0530

    up

---
+++ food-ordering-app/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React  from "react";
+
+
+const Home = () => {
+  return (
+   <>
+   <Navbar />
+
+   </>
+  );
+};
+
+export default Home;
---
+++ food-ordering-app/src/Pages/Success.jsx
@@ -0,0 +1,8 @@
+import React from  'react'
+
+const Sucess = () => {
+  return (
+    <div>Success</div>
+  )
+}
+export default Sucess;
